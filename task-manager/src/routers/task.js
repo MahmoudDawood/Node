b2e684977ffefe4948cccb84ee38a875098d32fd
Mahmoud Dawood <mm.dawood16@yahoo.com>
2021-12-26 18:17:44 +0200

    commit section-14

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -30,7 +30,7 @@ router.get('/tasks', auth, async (req, res) => {
 
     if (req.query.sortBy) {
         const parts = req.query.sortBy.split(':')
-        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1 // -1 for descending, 1 for ascending
     }
 
     try {
